62b76a133a0e124a7832e1396009cf9a80a24535
Michael Degginger <mdegginger@protonmail.com>
2024-12-29 20:45:00 -0700

    Initial Commit

---
+++ src/components/children/masonry-gallery.vue
@@ -0,0 +1,251 @@
+<template>
+  <div ref="masonry" class="masonry">
+    <div
+      v-for="image in filteredImages"
+      :key="image.src"
+      class="masonry-item"
+      @click="expandImage(image)"
+    >
+      <img :src="image.src" :alt="image.alt" />
+    </div>
+
+    <div v-if="selectedImageIndex !== null" class="expanded-overlay" @click="closeImage">
+      <div
+        class="expanded-container"
+        :class="{
+          'expanded-container-horizontal':
+            displayedImages[selectedImageIndex].orientation === 'horizontal',
+          'expanded-container-vertical':
+            displayedImages[selectedImageIndex].orientation === 'vertical',
+        }"
+      >
+        <img
+          :src="displayedImages[selectedImageIndex].src"
+          :alt="displayedImages[selectedImageIndex].alt"
+          class="expanded-image"
+          :class="displayedImages[selectedImageIndex].orientation"
+        />
+        <div class="expanded-text">{{ displayedImages[selectedImageIndex].description }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted, computed, watch, nextTick } from 'vue'
+
+interface Image {
+  src: string
+  alt: string
+  orientation: 'vertical' | 'horizontal'
+  type: string[]
+  description: string
+}
+
+const props = defineProps<{
+  images: Image[]
+  selectedType: string
+}>()
+
+const images = ref<Image[]>([])
+const displayedImages = ref<Image[]>([])
+const masonry = ref<HTMLDivElement | null>(null)
+const selectedImageIndex = ref<number | null>(null)
+const selectedType = ref<string>('')
+
+const fetchImages = async () => {
+  const response = await fetch('/images.json')
+  const data = await response.json()
+  images.value = data
+  console.log('Fetched Images:', images.value)
+  setImageOrientation()
+  shuffleImages()
+}
+
+const setImageOrientation = () => {
+  images.value.forEach((image) => {
+    const img = new Image()
+    img.src = image.src
+    img.onload = () => {
+      image.orientation = img.height > img.width ? 'vertical' : 'horizontal'
+    }
+  })
+}
+
+const shuffleImages = () => {
+  const shuffled = [...images.value].sort(() => Math.random() - 0.5)
+  displayedImages.value = shuffled.slice(0, images.value.length)
+}
+
+const arrangeMasonry = async () => {
+  const container = masonry.value
+  if (!container) return
+
+  container.style.position = 'relative'
+  container.style.height = 'auto'
+
+  const items = Array.from(container.children) as HTMLElement[]
+  const columnCount = Math.floor(container.clientWidth / 250)
+  const columnHeights = Array(columnCount).fill(0)
+
+  const gutter = 10
+  const columnWidth = Math.floor(container.clientWidth / columnCount) - gutter
+
+  items.forEach((item) => {
+    const minColumnIndex = columnHeights.indexOf(Math.min(...columnHeights))
+    const x = minColumnIndex * (columnWidth + gutter)
+    const y = columnHeights[minColumnIndex]
+
+    item.style.position = 'absolute'
+    item.style.width = `${columnWidth}px`
+    item.style.left = `${x}px`
+    item.style.top = `${y}px`
+
+    columnHeights[minColumnIndex] += item.offsetHeight + gutter
+  })
+
+  container.style.height = `${Math.max(...columnHeights)}px`
+}
+
+const filteredImages = computed(() => {
+  if (!props.selectedType || props.selectedType === 'All') {
+    console.log('Child Filtered Images:', filteredImages.value)
+    arrangeMasonry()
+    return props.images
+  }
+  console.log('Child Filtered Images:', filteredImages.value)
+  arrangeMasonry()
+  return props.images.filter((image) => image.type.includes(props.selectedType))
+})
+
+watch(filteredImages, async () => {
+  await nextTick()
+  arrangeMasonry()
+})
+
+const expandImage = (image: Image) => {
+  selectedImageIndex.value = displayedImages.value.findIndex((img) => img.src === image.src)
+}
+
+const closeImage = () => {
+  selectedImageIndex.value = null
+}
+
+watch(filteredImages, async () => {
+  await nextTick()
+  arrangeMasonry()
+})
+
+onMounted(async () => {
+  await fetchImages()
+
+  if (masonry.value) {
+    const imagesLoaded = Array.from(masonry.value.querySelectorAll<HTMLImageElement>('img')).map(
+      (img) => {
+        return new Promise<void>((resolve) => {
+          if (img.complete) resolve()
+          else {
+            img.onload = resolve
+            img.onerror = resolve
+          }
+        })
+      },
+    )
+
+    await Promise.all(imagesLoaded)
+  }
+
+  await nextTick()
+  setTimeout(arrangeMasonry, 5000)
+
+  window.addEventListener('resize', arrangeMasonry)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', arrangeMasonry)
+})
+</script>
+
+<style scoped>
+.masonry {
+  position: relative;
+  width: 100%;
+}
+
+.masonry-item {
+  position: absolute;
+  margin: 5px;
+  cursor: pointer;
+}
+
+.masonry-item img {
+  width: 100%;
+  display: block;
+  border-radius: 5px;
+  object-fit: contain;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+}
+
+.expanded-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.expanded-container {
+  background: white;
+  overflow: hidden;
+  padding: 10px 10px 50px;
+}
+
+.expanded-container-horizontal {
+  max-width: 50vw;
+  max-height: 75vh;
+}
+
+.expanded-container-vertical {
+  max-width: 30vw;
+  max-height: 80vh;
+}
+
+.expanded-text {
+  width: 100%;
+  height: 40px;
+  background: white;
+  color: black;
+  text-align: center;
+  font-size: 16px;
+  line-height: 40px;
+}
+
+@media screen and (max-width: 768px) {
+  .expanded-container-vertical,
+  .expanded-container-horizontal {
+    max-width: none;
+    max-height: none;
+  }
+}
+
+@media screen and (min-width: 1250px) {
+  .expanded-container-vertical {
+    max-width: 23vw;
+    max-height: 80vh;
+  }
+
+  .expanded-container-horizontal {
+    max-width: 38vw;
+    max-height: 75vh;
+  }
+}
+</style>
---
+++ src/components/outdoor-gallery.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="header-display"><h1>Michael Gets Outdoors</h1></div>
+  <div class="header-align">
+    <div role="radiogroup" aria-labelledby="view-type-group">
+      <div class="header-display" id="view-type-group">What do you want to see?</div>
+      <input type="radio" id="all" value="All" v-model="selectedType" />
+      <label for="all">All</label>
+      <input type="radio" id="views" value="Views" v-model="selectedType" />
+      <label for="views">Views</label>
+      <input type="radio" id="trails" value="Trails" v-model="selectedType" />
+      <label for="trails">Trails</label>
+      <input type="radio" id="signs" value="Signs" v-model="selectedType" />
+      <label for="signs">Signs</label>
+      <input type="radio" id="basenji" value="Basenji" v-model="selectedType" />
+      <label for="basenji">Basenjis</label>
+    </div>
+
+    <div role="radiogroup" aria-labelledby="view-mode-group">
+      <div class="header-display" id="view-mode-group">How do you want to view it?</div>
+      <input type="radio" id="masonry" value="Masonry" v-model="selectedView" />
+      <label for="masonry">Masonry</label>
+      <input type="radio" id="carousel" value="Carousel" v-model="selectedView" />
+      <label for="carousel">Carousel</label>
+    </div>
+  </div>
+  <component :is="currentComponent" :images="filteredImages" :selectedType="selectedType" />
+</template>
+
+<script setup lang="ts">
+import { ref, computed, onMounted } from 'vue'
+import Masonry from './children/masonry-gallery.vue'
+
+interface Image {
+  src: string
+  alt: string
+  orientation: 'vertical' | 'horizontal'
+  type: string[]
+  description: string
+}
+
+const images = ref<Image[]>([])
+const selectedType = ref<string>('All')
+const selectedView = ref<string>('Masonry')
+
+const fetchImages = async () => {
+  const response = await fetch('/images.json')
+  const data = await response.json()
+  images.value = data
+  setImageOrientation()
+}
+
+const setImageOrientation = () => {
+  images.value.forEach((image) => {
+    const img = new Image()
+    img.src = image.src
+    img.onload = () => {
+      image.orientation = img.height > img.width ? 'vertical' : 'horizontal'
+    }
+  })
+}
+
+const filteredImages = computed(() => {
+  if (!selectedType.value || selectedType.value === 'All') {
+    console.log('Parent Filtered Images:', filteredImages.value)
+
+    return images.value
+  }
+  console.log('Parent Filtered Images:', filteredImages.value)
+
+  return images.value.filter((image) => image.type.includes(selectedType.value))
+})
+
+const currentComponent = computed(() => (selectedView.value === 'Masonry' ? Masonry : Carousel))
+
+onMounted(() => {
+  fetchImages()
+})
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap');
+
+* {
+  font-family: 'Mulish', serif;
+  font-optical-sizing: auto;
+  font-weight: 500;
+  font-style: normal;
+}
+
+.header-display {
+  color: cornflowerblue;
+  font-weight: 600;
+  font-size: 20px;
+}
+
+.header-align {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 20px 0 30px;
+}
+
+::deep(.masonry-item) {
+  cursor: pointer !important;
+}
+</style>
